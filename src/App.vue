048a4f24870927060c674cdbe2e1b82475e04d3d
Marco Tienda <marco.tiendar@gmail.com>
2021-08-26 18:44:56 -0500

    Disenio de los componentes y funcionalidad de pokemon picture

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PokemonPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from './Pages/PokemonPage';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokemonPage
   }
 }
 </script>
