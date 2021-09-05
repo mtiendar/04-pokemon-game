221b333ece7e1b7a93ece97646769c2489d513de
Marco Tienda <marco.tiendar@gmail.com>
2021-09-04 19:30:04 -0500

    Prueba de que los componentes existan

--- tests/unit/pages/pokemonPage.spec.js
+++ tests/unit/pages/pokemonPage.spec.js
@@ -1,6 +1,6 @@
 import { shallowMount, mount } from '@vue/test-utils'
 import PokemonPage from '@/pages/PokemonPage'
-
+import { pokemons } from '../mocks/pokemons.mock'
 
 describe('PokemonPage Component', () => {
   let wrapper
@@ -17,5 +17,51 @@ describe('PokemonPage Component', () => {
     const mixPokemonArraySpy = jest.spyOn(PokemonPage.methods, 'mixPokemonArray')
     const wrapper = shallowMount(PokemonPage)
     expect(mixPokemonArraySpy).toHaveBeenCalled()
+  })
+
+  test('debe de hacer match con el snapshot cuando cargan los pokemons', () => {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemons,
+          pokemon: pokemons[0],
+          showPokemon: false,
+          showAnswer: false,
+          message: ''
+        }
+      },
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('debe de mostrar los componentes de pokemonPicture y pokemonOptions', () => {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemons,
+          pokemon: pokemons[0],
+          showPokemon: false,
+          showAnswer: false,
+          message: ''
+        }
+      },
+    })
+    
+    const picture = wrapper.find('pokemon-picture-stub')
+    const options = wrapper.find('pokemon-options-stub')
+
+    // PokemonPicture debe de existir
+    expect(picture.exists()).toBeTruthy()
+
+    // PokemonOptions debe de existir
+    expect(options.exists()).toBeTruthy()
+    
+    // PokemonPicture attribute pokemonid === '5'
+    expect(picture.attributes('pokemonid')).toBe('5')
+
+    // PokemonOptions attribute toBe true
+    expect(options.attributes('pokemons')).toBeTruthy()
+    
   })
 })
